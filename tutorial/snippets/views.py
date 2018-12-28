c2d7335dad8a06ed2a505a85282beb67cde48dd1
William Dana <william.b.dana@gmail.com>
2018-12-28 18:53:44 -0500

    Added User endpoints; WIP up to associating Snippets with Users in part 4

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,5 +1,6 @@
 from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer
+from django.contrib.auth.models import User
+from snippets.serializers import SnippetSerializer, UserSerializer
 from rest_framework import generics
 
 
@@ -11,3 +12,13 @@ class SnippetList(generics.ListCreateAPIView):
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
